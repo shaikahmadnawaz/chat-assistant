b237a76737b0e6b9c6f0b05a64c94caba74999b3
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-11-21 21:19:58 +0530

    updated font, now both chats in the conversation are aligned correctlty, changed theme white to dark

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,61 @@
 "use client";
-import { ArrowRight } from "lucide-react";
+
+import { SendHorizonal } from "lucide-react";
 import { useChat } from "ai/react";
 import { Button } from "@/components/ui/button";
 import AssistantIcon from "@/components/AssistantIcon";
 import UserIcon from "@/components/UserIcon";
+import { useEffect, useRef } from "react";
 export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/chat",
+  });
+  console.log(messages);
+
+  const messageEndRef = useRef<HTMLInputElement>(null);
+
+  const scrollToBottom = () => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <section className="w-full h-full flex flex-col dark:bg-gray-900">
-      <header className="p-4 bg-white shadow-md dark:bg-gray-800">
-        <h1 className="text-xl font-bold text-gray-900 dark:text-gray-100">
-          Chat Assistant
-        </h1>
+    <section className="min-h-screen w-full h-full flex flex-col bg-gray-900">
+      <header className="p-4 shadow-md bg-gray-800">
+        <h1 className="text-xl font-bold text-gray-100">Chat Assistant</h1>
       </header>
 
       <main className="flex-grow p-4 overflow-y-auto">
-        {messages
-          .filter((message) => message.role === "user")
-          .map((userMessage) => (
-            <div key={userMessage.id}>
+        {messages.map((message) => (
+          <div key={message.id}>
+            {message.role === "user" ? (
               <div className="flex items-end justify-end mb-4 space-x-2">
                 <span className="rounded-lg py-2 px-3 bg-green-500 text-white max-w-xs">
-                  {userMessage.content}
+                  {message.content}
                 </span>
                 <UserIcon />
               </div>
-            </div>
-          ))}
-
-        {messages
-          .filter((message) => message.role === "assistant")
-          .map((assistantMessage) => (
-            <div key={assistantMessage.id}>
+            ) : (
               <div className="flex items-end mb-4 space-x-2">
                 <AssistantIcon />
                 <span className="rounded-lg py-2 px-3 bg-blue-500 text-white max-w-xs">
-                  {assistantMessage.content}
+                  {message.content}
                 </span>
               </div>
-            </div>
-          ))}
+            )}
+          </div>
+        ))}
       </main>
 
-      <footer className="p-4 fixed inset-x-0 bottom-0 bg-white shadow-md dark:bg-gray-800">
+      <div ref={messageEndRef}></div>
+
+      <footer className="p-4 fixed inset-x-0 bottom-0 shadow-md bg-gray-800">
         <form className="flex items-center space-x-2" onSubmit={handleSubmit}>
           <input
-            className="flex-grow rounded-lg border-2 border-gray-300 p-2 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200"
+            className="flex-grow rounded-lg border-2 p-2 border-gray-600 bg-gray-700 text-white"
             placeholder="Continue your conversation..."
             type="text"
             value={input}
@@ -56,7 +66,7 @@ export default function Home() {
             type="submit"
             variant="default"
           >
-            <ArrowRight />
+            <SendHorizonal />
           </Button>
         </form>
       </footer>
